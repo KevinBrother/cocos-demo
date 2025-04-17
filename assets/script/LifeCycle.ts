f81cdcdb4e498b6f4c34b08dd2dd430f907e7e11
caojunjie <1301239018@qq.com>
2025-04-17 22:08:48 +0800

    lifeCycle

---
+++ assets/script/LifeCycle.ts
@@ -0,0 +1,45 @@
+import { _decorator, Component, Material, Node, MeshRenderer } from "cc";
+const { ccclass, property, executeInEditMode, requireComponent, menu } =
+  _decorator;
+
+@ccclass("LifeCycle")
+@requireComponent(MeshRenderer)
+@executeInEditMode(true)
+@menu("/good/next")
+export class LifeCycle extends Component {
+  private __init = false;
+
+  @property
+  public foo = 1;
+
+  @property(Material)
+  public ma: Material = null;
+
+  start() {
+    console.log("start");
+  }
+
+  update(deltaTime: number) {
+    console.log("update");
+  }
+
+  protected lateUpdate(dt: number) {
+    console.log("lateUpdate");
+  }
+
+  onLoad() {
+    console.log("onLoad");
+  }
+
+  onEnable() {
+    console.log("onEnable");
+  }
+
+  onDisable() {
+    console.log("onDisable");
+  }
+
+  onDestroy() {
+    console.log("onDestroy");
+  }
+}
