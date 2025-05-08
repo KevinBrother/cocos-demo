275aa7b37c8b1100db17d2d3b55a311ebbd99b1e
caojunjie <1301239018@qq.com>
2025-05-08 16:02:35 +0800

    push

---
+++ assets/script/READMD.md
@@ -0,0 +1,7 @@
+# gameManager
+
+## Description
+
+管理飞机与子弹的状态。
+
+飞机操作 在 UIMain 脚本中
--- assets/script/bullet01.ts
+++ assets/script/bullet01.ts
@@ -1,14 +1,28 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('bullet01')
+@ccclass("bullet01")
 export class bullet01 extends Component {
-    start() {
+  @property(Node)
+  public SelfPlane: Node = null;
 
-    }
+  @property(Node)
+  public speed = 1;
 
-    update(deltaTime: number) {
-        
+  start() {
+    this.node.setPosition(this.SelfPlane.position);
+  }
+
+  _emitBullet() {
+    // TODO 超出屏幕消失
+    this.node.setPosition(this.node.position.x, this.node.position.y, this.node.position.z - this.speed);
+    if (this.node.position.z < -100) {
+      this.node.destroy();
     }
+  }
+
+
+  update(deltaTime: number) {
+    this._emitBullet();
+  }
 }
-
